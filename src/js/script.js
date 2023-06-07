bda54d542174cb2a9b27fe38f6222f84da15e7de
Stanislav <ststymchyshyn@gmail.com>
2023-06-08 01:25:36 +0200

    half of the 84th lesson 08.06.2023

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,20 @@ const hamburger = document.querySelector(".hamburger"),
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
+  document.body.classList.add("menu-open");
+  document.body.style.overflow = "hidden";
 });
 
 closeElem.addEventListener("click", () => {
   menu.classList.remove("active");
+  document.body.classList.remove("menu-open");
+  document.body.style.overflow = "visible";
 });
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     menu.classList.remove("active");
+    document.body.classList.remove("menu-open");
+    document.body.style.overflow = "visible";
   }
 });
