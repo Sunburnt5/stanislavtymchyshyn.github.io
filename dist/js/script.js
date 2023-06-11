b481691a30c8b4385f711314fa1cd3952cface7f
Stanislav <ststymchyshyn@gmail.com>
2023-06-11 02:22:33 +0200

    final 2nd screen 11.06.2023 2:22

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,19 @@
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
   closeElem = document.querySelector(".menu__close");
+menuLinks = document.querySelectorAll(".menu__link");
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
   document.body.classList.add("menu-open");
   document.body.style.overflow = "hidden";
 });
+
+// menuLinks.addEventListener("click", () => {
+//   menu.classList.remove("active");
+//   document.body.classList.remove("menu-open");
+//   document.body.style.overflow = "visible";
+// });
 
 closeElem.addEventListener("click", () => {
   menu.classList.remove("active");
@@ -20,4 +27,17 @@ document.addEventListener("keydown", (event) => {
     document.body.classList.remove("menu-open");
     document.body.style.overflow = "visible";
   }
+});
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener("click", (event) => {
+    event.preventDefault(); // Забороняємо перехід за посиланням
+    const target = document.querySelector(event.target.hash); // Отримуємо цільовий елемент за допомогою хеша посилання
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" }); // Плавно прокручуємо до цільового елемента
+      menu.classList.remove("active"); // Закриваємо меню
+      document.body.classList.remove("menu-open");
+      document.body.style.overflow = "visible";
+    }
+  });
 });
